468eba652bb23d1199821c1ec16abcce8f9e7a80
Kevin Masters <irimiuke@gmail.com>
2024-05-27 14:48:21 -0400

    add in temporary http search submit like in portfolio

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,11 @@
 class FriendshipsController < ApplicationController
   def create
     friend = User.find(params[:friend])
-    current_user.friendships.build(friend_id: friend)
+    current_user.friendships.build(friend_id: friend.id)
     if current_user.save
       flash[:notice] = "Following friend"
     else
-      flash[:alert] = "There was something wrong"
+      flash[:alert] = "There was something wrong with the tracking request"
     end
     redirect_to my_friends_path
   end
